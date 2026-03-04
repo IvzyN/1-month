b32fcabe773665f4ac3b44700282c5ad9f571cd7
IvzyN <noelvimalstanly@gmail.com>
2026-03-04 07:20:07 -0500

    Create Games.jsx file for games menu page

---
+++ src/games/Games.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Games = () => {
+  return (
+    <div>
+      <h1>Games Menu</h1>
+      <ul>
+        <li><a href="/games/game1">Game 1</a></li>
+        <li><a href="/games/game2">Game 2</a></li>
+        <li><a href="/games/game3">Game 3</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Games;
